fad10bf3bedf83d1678097c86c1949e1f6dde435
QueenieAnnastasia <94047113+QueenieAnnastasia@users.noreply.github.com>
2025-05-10 23:37:29 +0700

    Chuong trinh nhap ten

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Nhập tên của bạn: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Xin chào " + name);
+    }
+}
